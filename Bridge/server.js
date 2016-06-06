b31427b2f35221ea4e280702fe31d6aad8d26509
Frank Steiler <frank@steilerdev.de>
2016-06-06 23:26:33 +0200

    Minor changes.

--- Bridge/server.js
+++ Bridge/server.js
@@ -1,3 +1,5 @@
 var IoTDevice = require("./IoTDevice.js");
 
-var device = new IoTDevice("FakeDevice", "FakeDevice_1", "n(1lx5sFsd3v1N4HJO");
+var device = new IoTDevice("FakeDevice", "FakeDevice_1", "n(1lx5sFsd3v1N4HJO", "192.168.10.50");
+device.connectPlatform();
+device.connectDevice();
